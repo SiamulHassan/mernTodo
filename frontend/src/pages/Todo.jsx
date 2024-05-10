e737ac4fc4bba1bc5e6717d754ea848a392c5410
SiamulHassan <syeam45@gmail.com>
2024-05-10 12:04:25 +0600

    edit and dnd done

--- frontend/src/pages/Todo.jsx
+++ frontend/src/pages/Todo.jsx
@@ -8,25 +8,51 @@ import {
   Divider,
   Checkbox,
 } from "antd";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import axios from "axios";
 import { MdDelete } from "react-icons/md";
 import Img from "../component/Img";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import Header from "../component/Header";
 import ModalForm from "../component/ModalForm";
-
+/// todo column
+const demoTodo = [
+  {
+    id: 0,
+    title: "Todo",
+    tasks: [],
+  },
+  {
+    id: 1,
+    title: "In Progress",
+    tasks: [],
+  },
+];
 const Todo = () => {
   const [showtodos, setShowTodos] = useState([]);
+
+  const [dragTodo, setDragTodo] = useState([]);
   const [form] = Form.useForm();
   const [api, contextHolder] = notification.useNotification();
   const user = useSelector((val) => val.login.value);
   // filtering todos
-  const completedTodos = showtodos?.filter((todos) => todos.isChecked === true);
-  const unFinishedTodos = showtodos?.filter(
-    (todos) => todos.isChecked === false
+  const completedTodos = useMemo(
+    () => showtodos?.filter((todos) => todos.isChecked === true),
+    [showtodos]
   );
+  const unFinishedTodos = useMemo(
+    () => showtodos?.filter((todos) => todos.isChecked === false),
+    [showtodos]
+  );
+
+  useEffect(() => {
+    const upTodo = [...demoTodo];
+    upTodo[0].tasks = unFinishedTodos;
+    setDragTodo(upTodo);
+  }, [unFinishedTodos]);
+
+  // console.log("drag:", dragTodo);
   // create todo
   const onFinish = async (values) => {
     await axios.post("http://localhost:8000/api/v1/todo", values);
@@ -66,12 +92,13 @@ const Todo = () => {
       duration: 0,
     });
   };
+
   /// dnd
   const onDragEnd = (result) => {
     const { source, destination } = result;
     if (!destination) return;
     if (source.droppableId !== destination.droppableId) {
-      const newData = [...JSON.parse(JSON.stringify(showtodos))];
+      const newData = [...JSON.parse(JSON.stringify(dragTodo))];
       const oldDroppableIndex = newData.findIndex(
         (x) => x.id == source.droppableId
       );
@@ -81,17 +108,17 @@ const Todo = () => {
       const [item] = newData[oldDroppableIndex].tasks.splice(source.index, 1);
       newData[newDroppableIndex].tasks.splice(destination.index, 0, item);
 
-      showtodos([...newData]);
+      setDragTodo([...newData]);
       //local storage saving
       localStorage.setItem("dragData", JSON.stringify([...newData]));
     } else {
-      const newData = [...JSON.parse(JSON.stringify(showtodos))]; //shallow copy concept
+      const newData = [...JSON.parse(JSON.stringify(dragTodo))]; //shallow copy concept
       const droppableIndex = newData.findIndex(
         (x) => x.id == source.droppableId
       );
       const [item] = newData[droppableIndex].tasks.splice(source.index, 1);
       newData[droppableIndex].tasks.splice(destination.index, 0, item);
-      showtodos([...newData]);
+      setDragTodo([...newData]);
       //local storage saving
       localStorage.setItem("dragData", JSON.stringify([...newData]));
     }
@@ -166,38 +193,77 @@ const Todo = () => {
         </div>
 
         <DragDropContext onDragEnd={onDragEnd}>
-          <div className="show-all-todo mt-16">
-            <h2 className="font-bold text-[15px]">In Progress</h2>
-          </div>
-          <div className="show-all-todo mt-16">
-            <h2 className="font-bold text-[15px] mb-5">Todo</h2>
-            {unFinishedTodos?.map((todo) => (
-              <div className="item-checkbox" key={todo._id}>
-                <div className="flex gap-4 items-center justify-between">
-                  <div className="flex gap-4">
-                    <Checkbox
-                      onChange={(e) => onChange(todo._id, e)}
-                    ></Checkbox>
-                    <div>
-                      <h2 className="font-bold text-[15px]">{todo.itemName}</h2>
-                      <p>{todo.itemDetails}</p>
+          <div className="show-all-todo mt-16"></div>
+          <div className="w-[900px] mt-16 flex gap-4 my-20 flex-col lg:flex-row">
+            {dragTodo?.map((val, index) => {
+              return (
+                <Droppable key={index} droppableId={`${val.id}`}>
+                  {(provided) => (
+                    <div
+                      className="p-5 lg:w-1/3 w-full bg-white  border-gray-400 border border-dashed"
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                    >
+                      <h2 className="text-center font-bold mb-6 text-black">
+                        {val.title}
+                      </h2>
+                      {val?.tasks?.map((component, index) => (
+                        <Draggable
+                          key={component._id}
+                          draggableId={`${val.id}-${component._id}`}
+                          index={index}
+                        >
+                          {(provided) => (
+                            <div
+                              className="bg-gray-200 mx-1 px-4 py-3 my-3"
+                              {...provided.dragHandleProps}
+                              {...provided.draggableProps}
+                              ref={provided.innerRef}
+                            >
+                              <div
+                                className="item-checkbox"
+                                key={component._id}
+                              >
+                                <div className="flex gap-4 items-center justify-between">
+                                  <div className="flex gap-4">
+                                    <Checkbox
+                                      onChange={(e) =>
+                                        onChange(component._id, e)
+                                      }
+                                    ></Checkbox>
+                                    <div>
+                                      <h2 className="font-bold text-[15px]">
+                                        {component.itemName}
+                                      </h2>
+                                      <p>{component.itemDetails}</p>
+                                    </div>
+                                  </div>
+                                  <div className="flex gap-2">
+                                    <MdDelete
+                                      onClick={() =>
+                                        handleDelete(component._id)
+                                      }
+                                      className="text-[25px] ml-auto text-[#f07272] cursor-pointer"
+                                    />
+                                    <ModalForm
+                                      editId={component._id}
+                                      editItemName={component.itemName}
+                                      editItemDetails={component.itemDetails}
+                                    />
+                                  </div>
+                                </div>
+                                <Divider className="my-[10px]" />
+                              </div>
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
                     </div>
-                  </div>
-                  <div className="flex gap-2">
-                    <MdDelete
-                      onClick={() => handleDelete(todo._id)}
-                      className="text-[25px] ml-auto text-[#f07272] cursor-pointer"
-                    />
-                    <ModalForm
-                      editId={todo._id}
-                      editItemName={todo.itemName}
-                      editItemDetails={todo.itemDetails}
-                    />
-                  </div>
-                </div>
-                <Divider className="my-[10px]" />
-              </div>
-            ))}
+                  )}
+                </Droppable>
+              );
+            })}
           </div>
         </DragDropContext>
 
@@ -235,3 +301,30 @@ const Todo = () => {
 };
 
 export default Todo;
+// {unFinishedTodos?.map((todo) => (
+//   <div className="item-checkbox" key={todo._id}>
+//     <div className="flex gap-4 items-center justify-between">
+//       <div className="flex gap-4">
+//         <Checkbox
+//           onChange={(e) => onChange(todo._id, e)}
+//         ></Checkbox>
+//         <div>
+//           <h2 className="font-bold text-[15px]">{todo.itemName}</h2>
+//           <p>{todo.itemDetails}</p>
+//         </div>
+//       </div>
+//       <div className="flex gap-2">
+//         <MdDelete
+//           onClick={() => handleDelete(todo._id)}
+//           className="text-[25px] ml-auto text-[#f07272] cursor-pointer"
+//         />
+//         <ModalForm
+//           editId={todo._id}
+//           editItemName={todo.itemName}
+//           editItemDetails={todo.itemDetails}
+//         />
+//       </div>
+//     </div>
+//     <Divider className="my-[10px]" />
+//   </div>
+// ))}
